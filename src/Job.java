0e4fbd15028bd12cd74cc2e977d6059e8130dd87
juhwan <jjeong20@amherst.edu>
2019-03-30 15:21:21 -0400

    added in getdeparture

--- src/Job.java
+++ src/Job.java
@@ -1,28 +1,32 @@
 public class Job {
-	// Juhwan Jeong and Ian McClaugherty, April 2019
+    // Juhwan Jeong and Ian McClaugherty, April 2019
 	/* The Job object keeps track of a job's arrival time, 
-		size, and departure time. */ 
+		size, and departure time. */
 
-	private double arrival; 
-	private double size; 
-	private double departure; 
+    private double arrival;
+    private double size;
+    private double departure;
 
-	public Job(double a, double s) {
-		arrival = a; 
-		size = s; 
-		departure = a + s; 
-	} // constructor 
+    public Job(double a, double s) {
+        arrival = a;
+        size = s;
+        departure = a + s;
+    } // constructor
 
-	public double getArrival() {
-		return arrival; 
-	} // getArrival
+    public double getArrival() {
+        return arrival;
+    } // getArrival
 
-	public double getSize() {
-		return size;
-	} // getSize
+    public double getSize() {
+        return size;
+    } // getSize
 
-	public void setDeparture(double time) {
-		departure = (time - arrival) + size; 
-	} // setDeparture
+    public void setDeparture(double time) {
+        departure = (time - arrival) + size;
+    } // setDeparture
+
+    public double getDeparture() {
+        return departure;
+    }
 
 } // Job class 
