7fa0a4e2596e8206804534d8fe59cc5adaf241f4
juhwan <jjeong20@amherst.edu>
2019-03-30 15:19:38 -0400

    wrote Hyperexponential

---
+++ src/HyperexponentialDistribution.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+
+public class HyperexponentialDistribution {
+    private double m1, m2, p;
+    private ExponentialDistribution exp1, exp2;
+    private Random r;
+
+    public HyperexponentialDistribution(double m1, double m2, double p) {
+        this.m1 = m1;
+        this.m2 = m2;
+        this.p = p;
+
+        exp1 = new ExponentialDistribution(m1);
+        exp2 = new ExponentialDistribution(m2);
+
+        r = new Random();
+    }
+
+    public double generate() {
+        if (r.nextDouble() <= p)
+            return exp1.generate();
+        else
+            return exp2.generate();
+    }
+
+    public double getMean() {
+        return p / m1 + (1 - p) / m2;
+    }
+
+    private double getXSquared() {
+        return 2 * p / (m1 * m1) + 2 * (1 - p) / (m2 * m2);
+    }
+
+    public double getVariance() {
+        return getXSquared() - Math.pow(getMean(), 2);
+    }
+}
