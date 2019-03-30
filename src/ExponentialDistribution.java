775b13b3b6fa059455cc699f5b5ed5a4aa9ce4db
juhwan <jjeong20@amherst.edu>
2019-03-30 15:12:20 -0400

    wrote Exponential

---
+++ src/ExponentialDistribution.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class ExponentialDistribution {
+
+    private double lambda;
+    private Random r;
+
+    public ExponentialDistribution(double lambda) {
+        this.lambda = lambda;
+        r = new Random();
+    }
+
+    public double generate() {
+        double area = r.nextDouble();
+        return -(Math.log(1 - area)) / lambda;
+    }
+
+    public double getMean() {
+        return 1 / lambda;
+    }
+
+    public double getVariance() {
+        return 1 / lambda / lambda;
+    }
+}
