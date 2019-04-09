2e52c67486fa7bbede3d704f631c3c9328fb76ce
Ian McClaugherty <ian.mcclaugherty@gmail.com>
2019-04-08 22:43:12 -0400

    Experiment2 changes
    Changed lambda to mu. Adjusted mu values.

--- src/Simulator2.java
+++ src/Simulator2.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.FileWriter;
 
 
-public class Experiment2 {
+public class Simulator2 {
     public static void main(String args[]) throws IOException {
         int number_of_jobs = 1000000;
 
@@ -14,12 +14,12 @@ public class Experiment2 {
         double l = 0;
 
         //records mean response times
-        double[][] mrt = new double[4][300];
-        ExponentialDistribution service_time = new ExponentialDistribution(1);
+        double[][] mrt = new double[4][100];
+        int[] variance = {1, 10, 20, 50};
 
-        while (l < 300) { // while loop allows us to easily compute lambda and store mean response times in mrt
-            double lambda = ((l + 1) * 0.01) +1; // incrementing lambda by 0.01 each iteration
-            
+        while (l < 100) { // while loop allows us to easily compute lambda and store mean response times in mrt
+            double lambda = ((l + 1) * 0.01) + 1; // incrementing lambda by 0.01 each iteration
+            ExponentialDistribution interarrival_time = new ExponentialDistribution(lambda);
 
             /* The following for loop computes our parameter values based upon the current variance. 
             An array of jobs is created. Arrival times are determined by the interarrival_time, which is 
@@ -32,16 +32,14 @@ public class Experiment2 {
             pairing is computed by taking the total response time (sum of all job response times) and dividing by 
             (the number of jobs - 10000). 
              */
-            double[] variance = {Math.pow(lambda,2), 10*Math.pow(lambda,2), 20*Math.pow(lambda,2), 50*Math.pow(lambda,2)};
             for (int v = 0; v < variance.length; v++) {
 
                 double var = variance[v];
                 //calculate p, m1, m2 based on variance
-                double p = (1 + Math.sqrt(1 - 4*Math.pow(lambda,2)/(2*var + 2*Math.pow(lambda,2)))) / 2;
-                double m1 = (2 * p)/lambda;
-                double m2 = (2-2*p)/lambda;
-                System.out.println("lambda: " + lambda + " p: " + p + " m1: " + m1 + " m2: " + m2);
-                HyperexponentialDistribution interarrival_time = new HyperexponentialDistribution(m1, m2, p);
+                double p = (1 + Math.sqrt(1 - 2 / (1 + var))) / 2;
+                double m1 = 2 * p;
+                double m2 = 2 * (1 - p);
+                HyperexponentialDistribution service_time = new HyperexponentialDistribution(m1, m2, p);
 
                 Job[] jobs = new Job[number_of_jobs];
 
@@ -69,10 +67,10 @@ public class Experiment2 {
             l += 1;
         }
 
-        write("variance_1_lambda_squared.csv", mrt[0]);
-        write("variance_10_lambda_squared.csv", mrt[1]);
-        write("variance_20_lambda_squared.csv", mrt[2]);
-        write("variance_50_lambda_squared.csv", mrt[3]);
+        write("variance_1.csv", mrt[0]);
+        write("variance_10.csv", mrt[1]);
+        write("variance_20.csv", mrt[2]);
+        write("variance_50.csv", mrt[3]);
     }
 
     private static void write(String filename, double[] x) throws IOException {
